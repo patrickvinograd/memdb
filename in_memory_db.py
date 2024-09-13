2de4116da4b00306edf3a711415c4576e2d10407
Patrick Vinograd <vinograd@gmail.com>
2024-09-13 11:17:12 -0700

    Clean up for readability

--- in_memory_db.py
+++ in_memory_db.py
@@ -1,53 +1,19 @@
 import sys
-from collections import defaultdict
+from collections import defaultdict, namedtuple
+
+Checkpoint = namedtuple('Checkpoint', ['value_state', 'count_state'])
 
 data = {}
 count_data = defaultdict(int)
 undo_log = []
 
-# For a set in a transaction, we need to record as state:
-# - the prior value v_old stored under k, which could be None
-# - the prior count of v_old, which could be 0
-# - the prior count of v_new, which could be 0
-# - and if we set k twice in a transaction:
-# - don't want to overwrite v_old
-# - save but don't overwrite count_v_old or count_v_new
-
-# SET a foo
-# COUNT foo # 1
-# BEGIN
-# SET a bar # state a:foo, c_foo:1, c_bar:0
-# COUNT foo # 0
-# COUNT bar # 1
-# SET a baz # state a:foo, c_foo:1, c_bar:0, c_baz:0
-# COUNT foo # 0
-# COUNT bar # 0
-# COUNT baz # 1
-# SET b baz # state a:foo, b:None, c_foo: 1, c_bar:0, c_baz:0
-# COUNT baz # 2
-# ROLLBACK
-# GET a # foo
-# GET b # NULL
-# COUNT foo # 1
-# COUNT bar # 0
-# COUNT baz # 0
-
-# For a delete in a transaction, we need to record as state:
-# - the existing value v_old stored under k, which could be None
-# - the prior count of v_old, which could be 0
-# If two deletes for the same v happen in a transaction:
-
-# SET a foo
-# SET b foo
-# BEGIN
-# DELETE a # state a:foo, c_foo: 2
-# DELETE b # state a:foo, b:foo, c_foo: 2
-# ROLLBACK
-# COUNT foo # 2
-
-
-def save_state(state, k, v=None):
-    value_state, count_state = state
+# Build an undo log for operations under a transaction
+# We need to save: the prior value v_old stored under k, the prior count of v_old,
+# and the prior count of new value v
+# Additionally if any of those have already been checkpointed in this transaction scope,
+# don't overwrite them. 
+def save_state(checkpoint, k, v=None):
+    value_state, count_state = checkpoint
     v_old = data.get(k, None)
     count_v_old = count_data[v_old]
 
@@ -66,10 +32,10 @@ def transact(f, args):
 
 def do_set(k, v):
     prev = data.get(k, None)
-    data[k] = v
-    count_data[v] = count_data[v] + 1
     if prev:
         count_data[prev] = count_data[prev] - 1
+    data[k] = v
+    count_data[v] = count_data[v] + 1
 
 def do_get(k):
     result = data.get(k, None)
@@ -85,27 +51,27 @@ def do_delete(k):
         count_data[v] = count_data[v] - 1
 
 def do_count(v):
-    print(count_data.get(v, 0))
+    print(count_data[v])
 
 def do_begin_txn():
-    undo_log.append(({}, {}))
+    undo_log.append(Checkpoint({}, {}))
 
 def do_rollback_txn():
     if len(undo_log) == 0:
         print("TRANSACTION NOT FOUND")
     else:
-        state = undo_log.pop()
-        for k, v in state[0].items():
+        checkpoint = undo_log.pop()
+        for k, v in checkpoint.value_state.items():
             data[k] = v
-        for k, v in state[1].items():
+        for k, v in checkpoint.count_state.items():
             count_data[k] = v
 
 def do_commit_txns():
     undo_log = []
 
 def parse_and_execute(command):
-    op, args = command.split()[0], command.split()[1:] 
-    #print(op, args)
+    tokens = command.split()
+    op, args = tokens[0], tokens[1:] 
     if op == 'SET':
         transact(do_set, args)
     elif op == 'GET':
@@ -124,7 +90,8 @@ def parse_and_execute(command):
         do_commit_txns()
     else:
         sys.stderr.write("Error, unrecognized command: " + str(op))
+        sys.exit(1)
 
-#with open(sys.stdin) as f:
-for command in sys.stdin:
-    parse_and_execute(command)
+if __name__ == '__main__':
+    for command in sys.stdin:
+        parse_and_execute(command)
